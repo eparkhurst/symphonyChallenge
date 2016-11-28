c02c0531496906762d74bef31e0f98e5974fcfed
eparkhurst <eli.parkhurst@gmail.com>
2016-11-28 11:52:46 -0800

    all items are shown on page with name, price in dollars and image. No styling

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 angular.module('Symphony',[]).controller('MainController',function($scope, requestService){
   requestService.getData().then(function(data){
-    $scope.test = data.extraInfo
     console.log(data);
+    $scope.test = data.extraInfo
+    $scope.items = data.products
   })
 })
