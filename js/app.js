758149881301a0641a688a42997d5f17e928014d
eparkhurst <eli.parkhurst@gmail.com>
2016-11-28 15:24:08 -0800

    added filters and orderby buttons. Also added some styling to the search aside

--- js/app.js
+++ js/app.js
@@ -1,9 +1,36 @@
 angular.module('Symphony',[]).controller('MainController',function($scope, requestService){
   requestService.getData().then(function(data){
-    console.log(data.bannerImage.ref);
     $scope.data = {}
     $scope.data.bannerImage  =data.bannerImage.ref
     $scope.data.pageTitle = data.pageTitle
     $scope.data.items = data.products
+    $scope.orderBy = "msrpInCents"
+    $scope.selectedTab = "msrpInCents"
+    $scope.range = {min:null,max:null}
+    $scope.changeOrder = function(order){
+      $scope.orderBy = order;
+      $scope.selectedTab = order
+    }
   })
+})
+.filter('rangeFilter', function() {
+  function filter( items, rangeInfo ) {
+    var filtered = [];
+    var max;
+    var min;
+    if(rangeInfo.max==null || rangeInfo.max.length ==0 ){
+      max = 1000
+    }else{
+      max = Number(rangeInfo.max);
+    }
+    min = Number(rangeInfo.min);
+    angular.forEach(items, function(item) {
+      if( item.msrpInCents/100 >= min && item.msrpInCents/100 <= max ) {
+          filtered.push(item);
+      }
+    });
+    return filtered;
+  };
+
+  return filter
 })
