718c12181a7c4666b40530cfdc1a69593d30e49d
eparkhurst <eli.parkhurst@gmail.com>
2016-11-28 12:38:13 -0800

    set up basic layout. still needs styling and media queries

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 angular.module('Symphony',[]).controller('MainController',function($scope, requestService){
   requestService.getData().then(function(data){
-    console.log(data);
-    $scope.test = data.extraInfo
-    $scope.items = data.products
+    console.log(data.bannerImage.ref);
+    $scope.data = {}
+    $scope.data.bannerImage  =data.bannerImage.ref
+    $scope.data.pageTitle = data.pageTitle
+    $scope.data.items = data.products
   })
 })
